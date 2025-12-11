46161059071dcc31c36226482e7ee77b1be85046
Colin Houlette <153432267+Colin2005H@users.noreply.github.com>
2025-12-11 10:42:56 +0100

    Schema flashcardsTable.

--- src/db/schema.js
+++ src/db/schema.js
@@ -4,11 +4,21 @@ import { timeStamp } from 'console'
 
 export const usersTable = sqliteTable('users', {
     id: text().primaryKey().$defaultFn(() => randomUUID()),
-    firstName: text().length(30).notNull(),
-    lastName: text().length(50).notNull(),
+    firstName: text().length(31).notNull(),
+    lastName: text().length(63).notNull(),
     email: text().unique().notNull(),
     password: text('password', {length: 255}).notNull(),
     role: text({enum: ['USER,', 'ADMIN']}).notNull().default('USER'),
-    createdAt: integer('created_at', {mode: 'timeStamp'}),
-    modifiedAt: integer('created_at', {mode: 'timeStamp'})
+    createdAt: integer('created_at', {mode: 'timestamp'}),
+    modifiedAt: integer('created_at', {mode: 'timestamp'})
+})
+
+export const flashcardsTable = sqliteTable('flashcards', {
+    id: text().primaryKey().$defaultFn(() => randomUUID()),
+    frontText: text().length(255).notNull(),
+    backText: text().length(255).notNull(),
+    frontURL: text().length(255),
+    backURL: text().length(255),
+    createdAt: integer('created_at', {mode: 'timestamp'}),
+    modifiedAt: integer('created_at', {mode: 'timestamp'})
 })
