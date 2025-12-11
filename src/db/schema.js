a945a5b936cf2c0c3f3260f146a34e97914a21f9
Colin Houlette <153432267+Colin2005H@users.noreply.github.com>
2025-12-11 10:55:12 +0100

    Schema collectionsTable.

--- src/db/schema.js
+++ src/db/schema.js
@@ -1,24 +1,36 @@
-import { sqlite, text, integer } from 'drizzle-orm/sqlite-core'
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 import { RandomUUID } from 'crypto'
 import { timeStamp } from 'console'
 
 export const usersTable = sqliteTable('users', {
     id: text().primaryKey().$defaultFn(() => randomUUID()),
-    firstName: text().length(31).notNull(),
-    lastName: text().length(63).notNull(),
+    firstName: text('first_name').length(31).notNull(),
+    lastName: text('last_name').length(63).notNull(),
     email: text().unique().notNull(),
     password: text('password', {length: 255}).notNull(),
     role: text({enum: ['USER,', 'ADMIN']}).notNull().default('USER'),
     createdAt: integer('created_at', {mode: 'timestamp'}),
-    modifiedAt: integer('created_at', {mode: 'timestamp'})
+    modifiedAt: integer('modified_at', {mode: 'timestamp'})
 })
 
 export const flashcardsTable = sqliteTable('flashcards', {
     id: text().primaryKey().$defaultFn(() => randomUUID()),
-    frontText: text().length(255).notNull(),
-    backText: text().length(255).notNull(),
-    frontURL: text().length(255),
-    backURL: text().length(255),
+    frontText: text('front_text').length(255).notNull(),
+    backText: text('back_text').length(255).notNull(),
+    frontURL: text('front_url').length(255),
+    backURL: text('back_url').length(255),
     createdAt: integer('created_at', {mode: 'timestamp'}),
     modifiedAt: integer('created_at', {mode: 'timestamp'})
+})
+
+export const collectionsTable = sqliteTable('collections',{
+    id: text().primaryKey().$defaultFn(() => randomUUID()),
+    userId: text('user_id').references(() => usersTable.id, {
+        onDelete: 'cascade',
+    }),
+    createdAt: integer('created_at', {mode: 'timestamp'}),
+    modifiedAt: integer('modified_at', {mode: 'timestamp'}),
+    title: text().length(31).notNull(),
+    description: text().length(255),
+    isPublic: integer('is_public',{ mode: 'boolean' })
 })
