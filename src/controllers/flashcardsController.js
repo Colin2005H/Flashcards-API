c3b8a893e5ff0a1bd909cdac63f3dc05743bbc3c
Paul8710 <paulletellierpro@gmail.com>
2025-12-17 14:52:11 +0100

    flashcards Fixes #5 Fix some imports.

--- src/controllers/flashcardsController.js
+++ src/controllers/flashcardsController.js
@@ -1,5 +1,6 @@
 import { db } from "../db/database.js"
 import { flashcardsTable } from "../db/schema.js"
+import { eq } from "drizzle-orm"
 import {request, response} from 'express'
 import 'dotenv/config'
 
@@ -57,21 +58,17 @@ export const updateFlashcards = async (req, res) => {
 export const deleteFlashcards = async (req, res) => {
 
     const { id } = req.params
-
-    if (!frontText || !backText || !frontURL || !backURL){
-        return res.status(400).send({ error: 'Invalid request'})
-    }
     
     try {
-        const Flashcard = await db.update(flashcardsTable).values({frontText: frontText, backText: backText, frontURL: frontURL, backURL: backURL, modifiedAt: Date()}).where(eq(flashcardsTable.id, id))
+        const Flashcard = await db.delete(flashcardsTable).where(eq(flashcardsTable.id, id))
         
         res.status(201).json({
-            message:'Flashcard updated successfully.', 
+            message:'Flashcard deleted successfully.', 
         })
     } catch (error) {
         console.error(error)
         res.status(500).json({
-            error: "Error while updating the flashcard.",
+            error: "Error while deleting the flashcard.",
         })
     }
 }
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import logger from './middleware/logger.js'
+import flashcardsRouter from './routers/flashcardsRoutes.js'
 
 const PORT = process.env.PORT || 3000
 
@@ -7,6 +8,8 @@ const app = express()
 
 app.use(express.json())
 app.use(logger)
+
+app.use('/flashcards', flashcardsRouter)
 
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`)
