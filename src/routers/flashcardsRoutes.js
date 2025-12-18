d73dcfa0ec5cd22385423cb82c6cab6257d69156
Paul8710 <paulletellierpro@gmail.com>
2025-12-18 08:17:37 +0100

    flashcards Fixes #5 Bug fixing.

--- src/routers/flashcardsRoutes.js
+++ src/routers/flashcardsRoutes.js
@@ -18,18 +18,23 @@ import {
 
 const router = Router()
 
+// Create a flashcard
 router.post('/', validateBody(flashcardBodySchema), createFlashcards)
 
+// Get a flashcard by ID
 router.get('/:id', validateParams(flashcardIdSchema), getFlashcard)
 
-router.get('/collection/:collectionId', validateParams(collectionIdSchema), listFlashcards)
+//router.get('/collection/:collectionId', validateParams(collectionIdSchema), listFlashcards)
 
-router.get('/collection/:collectionId/to-review', validateParams(collectionIdSchema), getFlashcardsToReview)
+//router.get('/collection/:collectionId/to-review', validateParams(collectionIdSchema), getFlashcardsToReview)
 
+// Update a flashcard
 router.put('/:id', validateParams(flashcardIdSchema), validateBody(flashcardBodySchema), updateFlashcards)
 
+// Add a review to a flashcard
 router.post('/:id/review', validateParams(flashcardIdSchema), validateBody(reviewBodySchema), reviewFlashcard)
 
+// Delete a flashcard
 router.delete('/:id', validateParams(flashcardIdSchema), deleteFlashcards)
 
 export default router
