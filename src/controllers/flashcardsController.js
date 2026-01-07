5885698f9d54d9666d6817c0bd0f8d82d5ef34e8
Paul8710 <paulletellierpro@gmail.com>
2026-01-07 21:03:31 +0100

    revisions Fixes #7 Need token

--- src/controllers/flashcardsController.js
+++ src/controllers/flashcardsController.js
@@ -1,6 +1,6 @@
 import { db } from "../db/database.js"
 import { flashcardsTable, revisionsTable } from "../db/schema.js"
-import { eq } from "drizzle-orm"
+import { and, eq } from "drizzle-orm"
 import 'dotenv/config'
 
 /**
@@ -78,27 +78,6 @@ export const listFlashcards = async (req, res) => {
     }
 }
 
-
-export const getFlashcardsToReview = async (req, res) => {
-    const { id } = req.params
-
-    try {
-        const flashcards = await db
-            .select()
-            .from(revisionsTable)
-            .where(eq(revisionsTable.collectionsId, id))
-        res.status(200).json({
-            flashcards,
-        })
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({
-            error: "Error while fetching flashcards to review.",
-        })
-    }
-}
-
-
 export const updateFlashcards = async (req, res) => {
     const { id } = req.params
     const { frontText, backText, frontURL, backURL } = req.body
@@ -159,7 +138,119 @@ export const deleteFlashcards = async (req, res) => {
     }
 }
 
-export const reviewFlashcard = async (req, res) => {
-    
+export const getFlashcardsToReview = async (req, res) => {
+    const userId = req.user.userId
+    console.log(userId);
+    try {
+        flashcardsToReview = [];
+        const flashcards = await db
+            .select()
+            .from(revisionsTable)
+            .where(eq(revisionsTable.userId, userId));
+        
+        for (let i = 0; i < flashcards.length; i++) {
+            const level = flashcards[i].level
+            let daysToReview = 0
 
+            if (level === 1) daysToReview = 1
+            else if (level === 2) daysToReview = 2
+            else if (level === 3) daysToReview = 4
+            else if (level === 4) daysToReview = 8
+            else daysToReview = 16
+
+            if(flashcards[i].lastReviewedAt + daysToReview <= Date.now()){
+                flashcardsToReview.push(flashcards[i])
+            }
+        }
+        res.status(200).json({
+            flashcardsToReview,
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            error: "Error while fetching flashcards to review.",
+        })
+    }
+}
+
+export const reviewFlashcard = async (req, res) => {
+    const { idFlashcard } = req.params
+    const userId = req.user.userId
+    try {
+        const updated = await db
+            .update(revisionsTable)
+            .set({
+                modifiedAt: new Date(),
+            })
+            .where(and(eq(revisionsTable.id, idFlashcard), eq(revisionsTable.userId, userId)))
+            .returning()
+
+        if (!updated) {
+            return res.status(404).json({
+                error: "Flashcard or User not found.",
+            })
+        }
+
+        res.status(200).json({
+            message:'Revision updated successfully.',
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            error: "Error while updating the revision.",
+        })
+    }
+
+
+}
+export const AddreviewFlashcard = async (req, res) => {
+    const { flashcardId } = req.params
+    const { level } = req.body
+    const userId = req.user.userId
+
+    try {
+        const revision = await db.insert(revisionsTable).values({
+            userId: userId,
+            flashcardId: flashcardId,
+            level: level,
+            createdAt: new Date(),
+            reviewedAt: new Date(),
+        }).returning()
+
+        res.status(201).json({
+            message:'Revision added successfully.',
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            error: "Error while creating the revision.",
+        })
+    }
+}
+
+export const deleteReviewFlashcards = async (req, res) => {
+    const { flashcardid } = req.params
+    const userId = req.user.userId
+
+    try {
+        const deleted = await db
+            .delete(revisionsTable)
+            .where(and(eq(revisionsTable.userId, userId), eq(revisionsTable.flashcardId, flashcardid)))
+            .returning()
+
+        if (!deleted) {
+            return res.status(404).json({
+                error: "Revision not found.",
+            })
+        }
+
+        res.status(200).json({
+            message:'Revision deleted successfully.',
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            error: "Error while deleting the revision.",
+        })
+    }
 }
--- src/routers/flashcardsRoutes.js
+++ src/routers/flashcardsRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { authenticateToken } from '../middleware/authenticateToken.js'
 import { validateBody, validateParams } from '../middleware/validation.js'
 import { 
     flashcardBodySchema, 
@@ -14,6 +15,8 @@ import {
     deleteFlashcards, 
     updateFlashcards,
     reviewFlashcard,
+    AddreviewFlashcard,
+    deleteReviewFlashcards
 } from '../controllers/flashcardsController.js'
 
 const router = Router()
@@ -24,15 +27,19 @@ router.post('/', validateBody(flashcardBodySchema), createFlashcards)
 // Get a flashcard by ID
 router.get('/:id', validateParams(flashcardIdSchema), getFlashcard)
 
-//router.get('/collection/:collectionId', validateParams(collectionIdSchema), listFlashcards)
+router.get('/collection/:collectionId', validateParams(collectionIdSchema), listFlashcards)
 
-//router.get('/collection/:collectionId/to-review', validateParams(collectionIdSchema), getFlashcardsToReview)
+// Get flashcards to review 
+router.get('/collection/to-review', authenticateToken, getFlashcardsToReview)
 
 // Update a flashcard
 router.put('/:id', validateParams(flashcardIdSchema), validateBody(flashcardBodySchema), updateFlashcards)
 
 // Add a review to a flashcard
-router.post('/:id/review', validateParams(flashcardIdSchema), validateBody(reviewBodySchema), reviewFlashcard)
+router.post('/:id/review', validateParams(flashcardIdSchema), validateBody(reviewBodySchema), AddreviewFlashcard)
+
+// Delete a Reviewflashcard 
+router.delete('/:id', authenticateToken, validateParams(flashcardIdSchema), deleteReviewFlashcards)
 
 // Delete a flashcard
 router.delete('/:id', validateParams(flashcardIdSchema), deleteFlashcards)
